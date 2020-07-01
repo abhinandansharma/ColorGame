77310b99e6a834ef88089f141caffb978aa3fa0c
abhinandan <abhinandan0659@gmail.com>
2020-07-01 19:24:58 +0530

    added basic logic

--- app.js
+++ app.js
@@ -8,8 +8,26 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[2]
+var displaySelected = document.getElementById("selected")
+var selectedColor 
+var messageDisplay = document.getElementById("message")
+
+displaySelected.textContent = pickedColor
 
 for(var i = 0; i < squares.length; i++) {
+    // add initial colors to squares
     squares[i].style.backgroundColor = colors[i]
+    // add Event Listeners to all squares
+    squares[i].addEventListener("click", function(){
+        selectedColor = this.style.backgroundColor
+        if( selectedColor === pickedColor){
+            messageDisplay.textContent = "Correct!"
+        }
+        else {
+            this.style.backgroundColor = "#1F1E1E";
+            messageDisplay.textContent = "Try Again"
+        }
+    })
 }
 
